7b8636dbcdb7876d00f426d0f33e93b1bb0b29aa
Oleksii Honchar <alekseyhonchar@gmail.com>
2022-08-12 18:31:24 +0100

    feat/add-inline-keyboard

---
+++ src/inline-keyboards.js
@@ -0,0 +1,8 @@
+export default class InlineKeyboards {
+  static peekWeek = [
+    [{text: 'current', callback_data: 0}],
+    [{text: 'next', callback_data: 1}],
+    [{text: 'custom', callback_data: 2}],
+  ];
+}
+
