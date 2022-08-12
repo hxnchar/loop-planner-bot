976e0bcc4bdda911b4a1ac39bb347114c4f1f031
Oleksii Honchar <alekseyhonchar@gmail.com>
2022-08-12 18:09:26 +0100

    feat/add-comands

---
+++ src/commands.js
@@ -0,0 +1,9 @@
+const commands = {
+  START: '/start',
+  NEW_SHIFTS: '/newshifts',
+  REMOVE_SHIFTS: '/removeshifts',
+  EDIT_SHIFT: '/editshifts',
+  SHOW_SHIFTS: '/shifts',
+};
+
+export default commands;
