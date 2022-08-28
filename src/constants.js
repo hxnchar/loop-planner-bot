feb854a415de0265d07cee18cbd698baec40cbc5
Oleksii Honchar <alekseyhonchar@gmail.com>
2022-08-28 14:10:07 +0100

    refactor/add-constants

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,14 @@ export default class Constants {
   static RESET_DAYS = 'reset-days';
   static SUBMIT_DAYS = 'submit-days';
   static SUBMIT_TIME = 'submit-time';
+  static SUBMIT_SETTINGS = 'submit-settings';
   static DATE = 'date';
   static DAY_SYMBOL = '⤴️';
   static NIGHT_SYMBOL = '⤵️';
+  static CHANGE_EVENT_NAME = 'change-event-name';
+  static CHANGE_EVENT_DURATION = 'change-event-duration';
+  static CHANGE_FIRST_SHIFT_START = 'change-first-shift-start';
+  static CHANGE_SECOND_SHIFT_START = 'change-second-shift-start';
+  static CHANGE_WAGE = 'change-wage';
+  static RETURN = 'return';
 }
